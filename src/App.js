a538816a926ea9a28f9ae53f12915a2dc0ae3f01
Zain Ghazanfar <69090613+ZainGhazanfarofficals@users.noreply.github.com>
2023-11-29 17:41:26 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Howl } from 'howler';
+import beepSoundFile from './beep-sound.mp3'; // Importing the beep sound file
+
+// Define the beep sound
+const beepSound = new Howl({
+  src: [beepSoundFile]
+});
 
 function App() {
+  // Function to play the beep sound for a specified duration
+  const playBeepForDuration = (duration) => {
+    const soundId = beepSound.play();
+
+    // Stop the sound after 'duration' milliseconds
+    setTimeout(() => {
+      beepSound.stop(soundId);
+    }, duration);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Beep Sound Example</h1>
+      {/* Play beep for 1000 milliseconds (1 second) when button is clicked */}
+      <button onClick={() => playBeepForDuration(70)}>Play Beep for 1 Second</button>
     </div>
   );
 }
